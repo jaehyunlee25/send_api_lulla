8e358c81be47e5fc90033ee7d7f246fbdd6be86f
jaehyunlee <jaehyunlee25@gmail.com>
2021-10-26 14:57:32 +0900

    renew

--- pages/api/send/push.js
+++ pages/api/send/push.js
@@ -1,10 +1,6 @@
-import {
-  RESPOND,
-  ERROR,
-  getUserIdFromToken,
-} from '../../../lib/apiCommon';
+import { RESPOND, ERROR, getUserIdFromToken } from '../../../lib/apiCommon';
 import setBaseURL from '../../../lib/pgConn'; // include String.prototype.fQuery
-import SMS from '../../../lib/sms';
+import PUSH from '../../../lib/push';
 
 /* const QTS = {
   // Query TemplateS
@@ -25,14 +21,14 @@ export default async function handler(req, res) {
   // #2. preflight 처리
   if (req.method === 'OPTIONS') return RESPOND(res, {});
 
-  setBaseURL('sqls/send/sms'); // 끝에 슬래시 붙이지 마시오.
+  setBaseURL('sqls/send/push'); // 끝에 슬래시 붙이지 마시오.
 
   // #3.1.
   try {
     return await main(req, res);
   } catch (e) {
     return ERROR(res, {
-      id: 'ERR.send.sms.3.2.2',
+      id: 'ERR.send.push.3.2.2',
       message: 'send server logic error',
       error: e.toString(),
     });
@@ -46,21 +42,32 @@ async function main(req, res) {
   if (qUserId.type === 'error') return qUserId.onError(res, '3.1');
   // const userId = qUserId.message;
 
-  const { message, phone } = req.body;
+  const { title, body, token, type, data } = req.body;
+  const message = {};
   // #3.2
-  const qSend = await SMS({
-    content: message,
-    phone,
-  });
-  if (qSend.type === 'error') return ERROR(res, {
-    message: 'SMS 전송에 실패했습니다.',
-    id: 'ERR.send.sms.3.2.1',
-    eStr: qSend.eStr,
-  });
+  if (type === 0) {
+    // 안드로이드
+    data.title = title;
+    data.body = body;
+    message.data = data;
+  } else {
+    data.sound = 'default';
+    message.data = data;
+    message.notification = { title, body };
+  }
+  message.token = token;
+
+  const qSend = await PUSH(message);
+  if (qSend.type === 'error')
+    return ERROR(res, {
+      message: 'PUSH 전송에 실패했습니다.',
+      id: 'ERR.send.push.3.2.1',
+      eStr: qSend.eStr,
+    });
 
   return RESPOND(res, {
     data: qSend.message,
-    message: 'SMS 전송에 성공했습니다.',
+    message: 'PUSH 전송에 성공했습니다.',
     resultCode: 200,
   });
 }
